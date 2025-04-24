8e3f60f9097c4b1b42c81201bc69f61528c118f5
danieldias-s <daniel.diass.2025@gmail.com>
2025-04-24 16:49:28 -0300

    alterado environment para publicar na vercel

--- Front-end/my-gestor/src/app/environment/environment.ts
+++ Front-end/my-gestor/src/app/environment/environment.ts
@@ -1,6 +1,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5221/api'
+    //apiUrl: 'http://localhost:5221/api'
+    apiUrl:'https://mygestor-api.onrender.com/api'
   };
   
