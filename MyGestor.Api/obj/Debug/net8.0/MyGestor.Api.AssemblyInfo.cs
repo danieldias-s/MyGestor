eea34b22c460409fe2a48466a78dd197b45f1c8c
danieldias-s <daniel.diass.2025@gmail.com>
2025-04-26 08:24:51 -0300

    Corrige caminho no Dockerfile

--- MyGestor.Api/obj/Debug/net8.0/MyGestor.Api.AssemblyInfo.cs
+++ MyGestor.Api/obj/Debug/net8.0/MyGestor.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyGestor.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea487eb8155ed27d5b29af53bf5737fe9090d2a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5622a42c4e92a1228f7e7ec6373d62e719ad23a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyGestor.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyGestor.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
