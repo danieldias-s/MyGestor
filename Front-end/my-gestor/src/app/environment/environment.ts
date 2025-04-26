967584a6780162e50aef9558cf464d79cf3d9cd1
danieldias-s <daniel.diass.2025@gmail.com>
2025-04-26 14:00:33 -0300

    alterado a rota do backend render

--- Front-end/my-gestor/src/app/environment/environment.ts
+++ Front-end/my-gestor/src/app/environment/environment.ts
@@ -2,6 +2,5 @@
 export const environment = {
     production: true,
     //apiUrl: 'http://localhost:5221/api'
-    apiUrl:'https://mygestor-api.onrender.com/api'
+    apiUrl:'https://mygestor-backend.onrender.com/api'
   };
-  
